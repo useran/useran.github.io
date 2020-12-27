3441602c11ece624365ef2d5b443d0704ce50b52
Userand <kozlovskiyandriy@gmail.com>
2020-12-27 17:00:29 +0200

    upd

--- classwork/2020-12-26/js/main.js
+++ classwork/2020-12-26/js/main.js
@@ -36,15 +36,22 @@ const count = arr => {
   });
   return total;
 }
-//4,5
+//4,5,6
 const mainEl = document.querySelector('.main');
+const wrapEl = document.querySelector('.wrap');
+const btnUpEl = document.querySelector('.btn-1');
+const btnDownEl = document.querySelector('.btn-2');
+const btnLeftEl = document.querySelector('.btn-3');
+const btnRightEl = document.querySelector('.btn-4');
 
+//function to create a div
 const addDiv = (idElem, str) => {
   let newDiv = document.createElement('div');
   newDiv.innerHTML = str;
   document.getElementById(idElem).appendChild(newDiv);
 }
 
+//function to create a field
 const matrDraw = arr => {
   let str = '';
   for (let i=0; i<arr.length; i++){
@@ -57,38 +64,75 @@ const matrDraw = arr => {
     str = ''; 
   }
 }
-mainEl.innerHTML = `Filled cells: ${count(newMatrix)}`;
 matrDraw(newMatrix);
-document.getElementById(`00`).classList.add('color');
+wrapEl.innerHTML = `Filled cells: ${count(newMatrix)}`;
 
+let currI;
+let currJ;
 
+//function to redraw the field
 const newRender = () => {
-  let currI;
-  let currJ;
   for (let i=0; i<newMatrix.length; i++){
     for (let j=0; j<newMatrix[0].length; j++){
       if (newMatrix[i][j] === 0) {
       document.getElementById(`${i}${j}`).classList.remove('color');
       } else { 
       document.getElementById(`${i}${j}`).classList.add('color');
+      currI = i;
+      currJ = j;
       }
     }
   }
+  wrapEl.innerHTML = `Filled cells: ${count(newMatrix)}`;
   clearArray(newMatrix);
 }
 
-mainEl.addEventListener('click', e => {
-  let iEl = e.target.id.charAt(0);
-  let jEl = e.target.id.charAt(1);
-  newMatrix[iEl][jEl] = 1;
-  newRender();
-});
-
+//calculating the move Right
 const moveRight = () => {
-  /* let currEl = newRender(); */
-  let iEl = Number(currPos.id.charAt(0));
-  let jEl = Number(currPos.id.charAt(1));
-  document.getElementById(`${iEl}${jEl+1}`).classList.add('color');
+  let i = currI;
+  let j = currJ;
+  if (j<newMatrix.length-1){
+  document.getElementById(`${i}${j+1}`).classList.add('color');
+  document.getElementById(`${i}${j}`).classList.remove('color');
+  currI = i;
+  currJ = j+1;
+  } else alert('No other way!');
+}
+
+//calculating the move Left
+const moveLeft = () => {
+  let i = currI;
+  let j = currJ;
+  if (j>0){
+  document.getElementById(`${i}${j-1}`).classList.add('color');
+  document.getElementById(`${i}${j}`).classList.remove('color');
+  currI = i;
+  currJ = j-1;
+  } else alert('No other way!');
+}
+
+//calculating the move UP
+const moveUp = () => {
+  let i = currI;
+  let j = currJ;
+  if (i>0){
+  document.getElementById(`${i-1}${j}`).classList.add('color');
+  document.getElementById(`${i}${j}`).classList.remove('color');
+  currI = i-1;
+  currJ = j;
+  } else alert('No other way!');
+}
+
+////calculating the move Down
+const moveDown = () => {
+  let i = currI;
+  let j = currJ;
+  if (i<newMatrix.length - 1){
+  document.getElementById(`${i+1}${j}`).classList.add('color');
+  document.getElementById(`${i}${j}`).classList.remove('color');
+  currI = i+1;
+  currJ = j;
+  } else alert('No other way!');
 }
 
 //keyboard listener
@@ -108,3 +152,15 @@ document.addEventListener('keydown', e => {
       break;
   }
 })
+
+mainEl.addEventListener('click', e => {
+  let iEl = e.target.id.charAt(0);
+  let jEl = e.target.id.charAt(1);
+  newMatrix[iEl][jEl] = 1;
+  newRender();
+});
+
+btnUpEl.addEventListener('click', moveUp);
+btnDownEl.addEventListener('click', moveDown);
+btnLeftEl.addEventListener('click', moveLeft);
+btnRightEl.addEventListener('click', moveRight);
