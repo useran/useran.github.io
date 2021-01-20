c6547a9ac4518bf3189b16c696e91187607eb38a
Userand <kozlovskiyandriy@gmail.com>
2021-01-20 22:54:39 +0200

    upd

--- Node_start/homework/18.01.2021/index.js
+++ Node_start/homework/18.01.2021/index.js
@@ -6,36 +6,37 @@ request('https://dou.ua/', function(error, response, body) {
       console.log('error:', error);
   } else {
     let str = body;
-    let posEnd = 'announces';
+    let strEndSearch = 'ua/img/announces';
     let txtContainer = [];
+    let qty = str.lastIndexOf(strEndSearch);
 
-    const retrieveStr = (strForSearch) => {
+    const retrieveStr = (strStartSearch) => {
       let position = 0;
       let indexStart = 0;
       let indexEnd = 0;
-      for (let i = 0; i < 10; i += 1) {
-        indexStart = str.indexOf(strForSearch, position);
-        indexEnd = str.indexOf(posEnd, indexStart+70);
-        txtContainer[i] = str.slice(indexStart+19, indexEnd);
-        position = indexStart + 100;
+      let i = 0;
+      while (indexEnd < qty){
+        indexStart = str.indexOf(strStartSearch, position);
+        indexEnd = str.indexOf(strEndSearch, indexStart);
+        txtContainer[i] = str.slice(indexStart, indexEnd);
+        position = indexStart+1;
+        i += 1;
       }
       return txtContainer;
     }
     retrieveStr('loading="lazy"');
 
-    const parseArr = [];
-    const parseArr2 = [];
-    const parseArr3 = [];
+    let parseArr = [];
+    let parseArr2 = [];
 
-    for (let i = 0; i < 10; i += 1) {
-      parseArr.push(txtContainer[i].split(' '));
-      parseArr2.push(parseArr[i][0].split('/'));
-      parseArr3.push(parseArr[i][0].split('"'));
+    for (let i = 0; i < txtContainer.length; i += 1) {
+      parseArr = txtContainer.map(e => e.slice(e.indexOf('https'), e.indexOf('"', e.indexOf('" srcset'))));
+      parseArr2 = parseArr.map(e => e.split('/'));
     }
-
-    for (let i = 0; i < 10; i += 1) {
-      let name = parseArr2[i][parseArr2[i].length-1].slice(0, -1);
-      request(parseArr3[i][1]).pipe(fs.createWriteStream(`./images/${name}`));
+    for (let i = 0; i < txtContainer.length; i += 1) {
+      let name = parseArr2[i][parseArr2[i].length-1];
+      console.log(name);
+      request(parseArr[i]).pipe(fs.createWriteStream(`./images/${name}`));
     }
   }
-});
+}); 
